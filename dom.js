10453961202fc846d37e0ef1a54ef629ff2d9f54
Sumit0397 <sumitmajumdar16@gmail.com>
2023-04-21 12:44:36 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,12 @@
+// console.log(document);
+//console.log(document.all);
+console.log(document.title);
+document.title = 'iTEMS';
+console.log(document.title);
+const header = document.getElementById('header-title');
+header.style.border = 'solid 3px black';
+
+
+const element = document.querySelector('h2');
+element.style.fontWeight = 'bold';
+element.style.color = 'green';
